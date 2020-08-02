4a6b81b35c42631b09be9c2982542b5e0204b180
Mikita Chabatarovich <mikita.chabatarovich@gmail.com>
2020-08-02 11:20:22 +0200

    added ball

--- Game.hpp
+++ Game.hpp
@@ -3,13 +3,13 @@
 
 #include <SFML/Graphics.hpp>
 
-
 #include <iostream>
+#include <cmath>
 
 
 const auto TITLE = "SFML Haxball";
 const auto PLAYER_SIZE = 30;
-
+const auto BALL_SIZE = 20;
 
 struct Game {
     sf::RenderWindow* window;
@@ -19,6 +19,10 @@ struct Game {
     sf::CircleShape player;
     float moveSpeed{0.1};
 
+    sf::CircleShape ball;
+    sf::Vector2f ballDirection;
+    float ballSpeed{0.07};
+
     Game(sf::RenderWindow* window, sf::Vector2u windowSize) : window(window), windowSize(windowSize){
         window->create(sf::VideoMode(windowSize.x, windowSize.y), TITLE);
         initScene();
@@ -26,9 +30,17 @@ struct Game {
 
     void initScene(){
         frame = Frame{window};
+        
         player = sf::CircleShape(PLAYER_SIZE);
         auto center = sf::Vector2f(window->getSize().x / 2, window->getSize().y / 2);
         player.setPosition(sf::Vector2f(center.x - player.getRadius(), center.y - player.getRadius()));
+
+        ball = sf::CircleShape(BALL_SIZE);
+        float ballOffset = 60;
+        ball.setPosition(
+            sf::Vector2f(center.x - player.getRadius() + ballOffset, center.y - player.getRadius() + ballOffset));
+        ballDirection = sf::Vector2f(1.0f, 0.0f);
+        ball.setFillColor(sf::Color::Blue);
     }
 
     void handleWindowEvents(){
@@ -46,9 +58,9 @@ struct Game {
         }
     }
 
-    bool isPlayerCollidingWithWalls(){
+    bool isCollidingWithWalls(sf::CircleShape cs){
         for (const auto wall : frame.walls){
-            if (player.getGlobalBounds().intersects(wall.getGlobalBounds())){
+            if (cs.getGlobalBounds().intersects(wall.getGlobalBounds())){
                 return true;
             } 
         }
@@ -59,9 +71,33 @@ struct Game {
         sf::Vector2f moveVector = getMoveVector();
         sf::Vector2f oldPosition = player.getPosition();
         player.move(moveVector);
-        if (isPlayerCollidingWithWalls()) {
+        
+        if (isCollidingWithWalls(player)) {
             player.setPosition(oldPosition);
         }
+
+        sf::Vector2f ballOldPosition = ball.getPosition();
+        ball.move(ballDirection.x * ballSpeed, ballDirection.y * ballSpeed);
+
+        if (isCollidingWithWalls(ball)){
+            ball.setPosition(ballOldPosition);
+            ballDirection = sf::Vector2f(ballDirection.x * -1, ballDirection.y * -1);
+        }
+
+        if(player.getGlobalBounds().intersects(ball.getGlobalBounds())) {
+            ball.setPosition(ballOldPosition);
+            player.setPosition(oldPosition);
+            // ballDirection = sf::Vector2f(ballDirection.x * -1, ballDirection.y);
+            ballDirection = getDirectionVector(ballDirection, moveVector);
+
+            std::cout << "move vector";
+            vecutils::printVector<decltype(moveVector)>(moveVector);
+        }
+    }
+
+    sf::Vector2f getDirectionVector(sf::Vector2f v1, sf::Vector2f v2){
+        auto magnitude = std::sqrt(std::pow((v2.x - v1.x), 2) + std::pow(v2.y - v1.y ,2));
+        return sf::Vector2f((v2.x - v1.x) / magnitude,  (v2.y - v1.y) / magnitude);
     }
 
     sf::Vector2f getMoveVector(){
@@ -99,8 +135,9 @@ struct Game {
 
     void Draw(){
         window->clear();
-        window->draw(frame);
         window->draw(player);
+        window->draw(frame);
+        window->draw(ball);
         window->display();
     }
 
